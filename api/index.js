b3372d00e7d9c831122e8b902bda14abf92da3df
Ahmed Yahia <a7medya7ia123@gmail.com>
2024-08-09 18:20:34 +0300

    create user model and test api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
+const app = express();
+app.use("/api/user", userRouter);
 const db = mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("connect to db successfully"))
   .catch((error) => console.log(error));
 
-const app = express();
+app.get("/", (req, res) => {
+  res.send("hello, world");
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
