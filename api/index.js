a87feb80095dc99b1ddc5fb46120ddfbb3e5430d
Ahmed Yahia <a7medya7ia123@gmail.com>
2024-08-10 16:18:58 +0300

    create sign up api route and middleware for possible errors

--- api/index.js
+++ api/index.js
@@ -2,9 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 const db = mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("connect to db successfully"))
